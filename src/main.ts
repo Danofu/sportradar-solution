c413c7e22d53dabef3adf7b5e177cf18cf4d0a5d
Daniil Borysov <danofu13@gmail.com>
2023-02-22 11:25:45 +0100

    Setup dotenv library

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from 'src/app.module';
 
+const PORT = parseInt(process.env.PORT) || 3000;
+const logger = new Logger('bootstrap');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => logger.log(`Application started at port: ${PORT}`));
 }
 bootstrap();
